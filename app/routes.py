d637de7b4622a01fde2ebf18455c14a2236a95b1
Arom Jhee <aromjhee@gmail.com>
2020-07-21 15:21:29 -0700

    form submit

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 import os
 import psycopg2
 from app.forms import AppointmentForm
+from datetime import datetime
 
 bp = Blueprint('main', __name__, url_prefix='')
 
@@ -15,6 +16,20 @@ CONNECTION_PARAMETERS = {
 @bp.route('/', methods=['GET', 'POST'])
 def main():
     form = AppointmentForm()
+    if form.validate_on_submit():
+        with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+            with conn.cursor() as curs:
+                curs.execute('''
+                    INSERT INTO appointments (name, start_datetime, end_datetime, description, private)
+                    VALUES (%(name)s, %(start_datetime)s, %(end_datetime)s, %(description)s, %(private)s)
+                    ''',
+                    {'name': form.name.data,
+                    'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
+                    'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
+                    'description': form.description.data,
+                    'private': form.private.data}
+                )
+        return redirect('/')
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         with conn.cursor() as curs:
             curs.execute('''
