2e45891aba6b8715c965d1d5671ddc1a92321496
Arom Jhee <aromjhee@gmail.com>
2020-07-21 15:06:05 -0700

    form date and time input

--- app/forms.py
+++ app/forms.py
@@ -3,13 +3,18 @@ from wtforms import (
     StringField, DateField, TimeField, TextAreaField, BooleanField, SubmitField
 )
 from wtforms.validators import DataRequired
+from wtforms.widgets.html5 import DateInput, TimeInput
 
 class AppointmentForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
-    start_datetime = DateField('start_date', validators=[DataRequired()])
-    start_datetime = TimeField('start_time', validators=[DataRequired()])
-    end_datetime = DateField('end_date', validators=[DataRequired()])
-    end_datetime = TimeField('end_time', validators=[DataRequired()])
+    start_date = DateField('start_date', validators=[
+                        DataRequired()], widget=DateInput())
+    start_time = TimeField('start_time', validators=[
+                        DataRequired()], widget=TimeInput())
+    end_date = DateField('end_date', validators=[
+                         DataRequired()], widget=DateInput())
+    end_time = TimeField('end_time', validators=[
+                         DataRequired()], widget=TimeInput())
     description = TextAreaField('description', validators=[DataRequired()])
     private = BooleanField('private')
     submit = SubmitField('Save')
